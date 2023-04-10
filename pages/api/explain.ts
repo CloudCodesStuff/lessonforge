f8f19650fc96036033c2277da6a53f4fe5e6749d
CloudCodes <85653592+CloudCodesStuff@users.noreply.github.com>
2023-04-09 20:23:35 -0400

    changed to chatgpt

--- pages/api/explain.ts
+++ pages/api/explain.ts
@@ -24,50 +24,51 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse<
     const lessonPlan: LessonPlan = req.body
     console.log('lessonPlan', lessonPlan)
 
-    const response = await openai.createCompletion({
-      model: 'text-davinci-003',
-      prompt: `
-        Create a detailed lesson plan about ${lessonPlan.topic} with the following format. 
-        The grade level should be ${lessonPlan.level} and the description is ${lessonPlan.description}. 
-        The class is ${lessonPlan.length} minutes long. You may add items as necessary to each category. 
-        Make sure you bold the categories using <strong></strong> and finish filling out every sentence! 
-        MAKE SURE YOU RESPOND IN BULLETS, NOT SENTENCES. Make sure the lesson does not go over the class length.
-        Explain carefully and be detailed about every step. Be sure to add more steps. 
+    const response = await openai.createChatCompletion({
+      model: "gpt-3.5-turbo",
+      messages: [{"role": "user", "content":   `Create a detailed lesson plan about ${lessonPlan.topic} with the following format. 
+      The grade level should be ${lessonPlan.level} and the description is ${lessonPlan.description}. 
+      The class is ${lessonPlan.length} minutes long. You may add items as necessary to each category. 
+      Make sure you bold the categories using <strong></strong> and finish filling out every sentence! 
+      MAKE SURE YOU RESPOND IN BULLETS, NOT SENTENCES. Make sure the lesson does not go over the class length.
+      Explain carefully and be detailed about every step. Be sure to add more steps. 
 
-        Topic: ${lessonPlan.topic}
+      Topic: ${lessonPlan.topic}
 
-        Learning objectives:
-        - 
+      Learning objectives:
+      - 
 
-        Materials:
-        - Pencil
-        - Paper
-        - Blah
+      Materials:
+      - Pencil
+      - Paper
+      - Blah
 
-        Intro (duration):
-        - 
-        - 
-        - 
-        - 
+      Intro (duration):
+      - 
+      - 
+      - 
+      - 
 
-        Procedure (duration):
-        - 
-        - 
-        - 
-        - 
+      Procedure (duration):
+      - 
+      - 
+      - 
+      - 
 
-        Assessment (duration):
-        - 
-        - 
-        - 
-        - 
+      Assessment (duration):
+      - 
+      - 
+      - 
+      - 
 
-        Conclusion (duration):
-        - 
-        - 
-        - 
-        - 
-      `,
+      Conclusion (duration):
+      - 
+      - 
+      - 
+      - 
+    `}],
+    
+
       temperature: 0.85,
       max_tokens: 700,
       top_p: 1,
@@ -75,7 +76,8 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse<
       presence_penalty: 0,
     })
 
-    const suggestion = response.data?.choices?.[0].text
+    const suggestion = response.data.choices[0].message?.content
+    console.log(suggestion)
 
     if (suggestion === undefined) throw new Error('No suggestion found')
     res.setHeader('Content-Type', 'text/html')
@@ -83,6 +85,6 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse<
     res.status(200).json({ result: suggestion })
   } catch (error) {
     console.error(error)
-    if (error) throw new Error('Something happened on our end')
+    if (error) throw new Error('Something happened on our end. Error:'+error)
   }
 }
